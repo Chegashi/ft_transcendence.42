0d024d8e175d0e4c29a2189276fde521b30cddbc
chegashi <mochegri@student.1337.ma>
2022-09-29 00:19:34 +0100

    bsht

--- ping_pong/src/App.tsx
+++ ping_pong/src/App.tsx
@@ -1,13 +1,15 @@
-// import React from 'react';
+import React from 'react';
 import './App.css';
 import Game from './game'
 
 function App() {
+  React.useState(0);
+  // let height:number = √
   return (
     <div className="App">
       <header className="App-header">
       {<Game />}
-      </header>
+      </header> 
     </div>
   );
 }
