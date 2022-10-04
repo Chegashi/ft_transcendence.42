ba2cf44dcf0b578e02f508868728d97a4036f3a6
chegashi <mochegri@student.1337.ma>
2022-10-04 01:51:16 +0100

    edit

--- _inti.sh
+++
@@ -1,4 +0,0 @@
-`curl -fsSL https://raw.githubusercontent.com/hakamdev/42homebrew/master/install.sh | zsh`
-`curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | zsh`
-# `npx create-react-app pong`
-# npm start
