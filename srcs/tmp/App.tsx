f1525af1a9eadfb51d51e197c27e96633766965c
chegashi <mochegri@student.1337.ma>
2022-12-05 05:53:43 +0000

    add allInOnee

---
+++ srcs/tmp/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Game from './game';
+
+function App() {
+  return (
+    <div className="App">
+    <Game width={600} height={400}/>
+    </div>
+  );
+}
+
+export default App;
